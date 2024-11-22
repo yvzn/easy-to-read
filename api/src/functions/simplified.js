8fefba392c4cc2e57f8368665dc055428c84152c
Yvan Razafindramanana <mail@ludeo.net>
2024-11-22 15:02:30 +0000

    Call LLM from API

--- api/src/functions/simplified.js
+++ api/src/functions/simplified.js
@@ -1,13 +1,29 @@
 const { app } = require('@azure/functions');
+const OpenAI = require("openai");
+
+const token = process.env["GITHUB_TOKEN"];
+const endpoint = "https://models.inference.ai.azure.com";
+const modelName = "gpt-4o-mini";
 
 app.http('simplified', {
-	methods: ['GET', 'POST'],
+	methods: ['POST'],
 	authLevel: 'anonymous',
 	handler: async (request, context) => {
-		context.log(`Http function processed request for url "${request.url}"`);
+		context.info(`Http function processed request for url "${request.url}"`);
 
-		const name = request.query.get('name') || await request.text() || 'world';
+		const client = new OpenAI({ baseURL: endpoint, apiKey: token });
 
-		return { body: `Hello, ${name}!` };
+		const response = await client.chat.completions.create({
+			messages: [
+				{ role: "system", content: "You are a helpful assistant." },
+				{ role: "user", content: "What is the capital of France?" }
+			],
+			temperature: 1.0,
+			top_p: 1.0,
+			max_tokens: 1000,
+			model: modelName
+		});
+
+		return { body: response.choices[0].message.content };
 	}
 });
