3b4201f81e35e8de1238ff65a97d4649218a622b
Yvan Razafindramanana <mail@ludeo.net>
2024-12-04 07:13:49 +0100

    Response as HTML

--- api/src/functions/simplified.js
+++ api/src/functions/simplified.js
@@ -10,34 +10,68 @@ app.http('simplified', {
 		context.info(`Http function processed request for url "${request.url}"`);
 
 		try {
-			const input = await request.text();
+			const requestBody = await request.text();
+			const requestParams = new URLSearchParams(requestBody);
 
-			const chunks = await getChatResponse(input);
+			const userInput = requestParams.get("t");
+			const debug = requestParams.get("d") === "true";
+
+			const responseStream = toHtml(simplify(userInput, debug));
 
 			return {
-				body: Readable.from(chunks),
-				headers: { "Content-Type": "text/plain" },
+				body: Readable.from(responseStream),
+				headers: { "Content-Type": "text/html;charset=utf-8" },
 			}
 		} catch (error) {
 			context.error(error);
 
 			return {
 				status: 503,
+				headers: { "Content-Type": "text/plain;charset=utf-8" },
 				body: "Service has failed to process the request. Please try again later.",
 			};
 		}
 	}
 });
 
+const htmlTemplatePromise = readResource("template.html");
+
+async function* toHtml(source) {
+	const htmlTemplate = await htmlTemplatePromise;
+	const [htmlHeader, htmlFooter] = htmlTemplate.split("{0}");
+
+	yield htmlHeader;
+
+	for await (const chunk of source) {
+		yield chunk.replace('<', '&lt;').replace('>', '&gt;');
+	}
+
+	yield htmlFooter;
+}
+
+async function* simplify(text, debug) {
+	const responseStream = getModelResponse(text);
+
+	for await (const chunk of responseStream) {
+		yield chunk || '';
+		if (debug) {
+			yield "\n";
+		}
+	}
+}
+
 const token = process.env["GITHUB_TOKEN"];
 const endpoint = "https://models.inference.ai.azure.com";
 const modelName = "gpt-4o-mini";
 
-async function* getChatResponse(userInput) {
+const systemPromptPromise = readResource("system-prompt.txt");
+const userPromptPromise = readResource("user-prompt.txt");
+
+async function* getModelResponse(userInput) {
 	const client = new OpenAI({ baseURL: endpoint, apiKey: token });
 
-	const systemPrompt = await readResource("system-prompt.txt");
-	let userPrompt = await readResource("user-prompt.txt");
+	const systemPrompt = await systemPromptPromise;
+	let userPrompt = await userPromptPromise;
 	userPrompt = userPrompt.replace("{0}", userInput);
 
 	const stream = await client.chat.completions.create({
@@ -54,7 +88,6 @@ async function* getChatResponse(userInput) {
 
 	for await (const part of stream) {
 		yield part.choices[0]?.delta?.content || '';
-		yield '|';
 	}
 }
 
