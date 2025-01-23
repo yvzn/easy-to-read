6180a9cedb87b3b8c8a8289beba84e3f88408fda
Yvan Razafindramanana <mail@ludeo.net>
2025-01-23 09:24:23 +0100

    Hide small CTA without JavaScript

--- site/js/cta.js
+++ site/js/cta.js
@@ -1,11 +1,6 @@
 (function () {
   const smallCta = document.getElementById('cta-small');
   const largeCta = document.getElementById('cta-large');
-  const simplifyButton = document.getElementById('simplify-btn');
-
-  if (simplifyButton || largeCta) {
-    smallCta.style.display = 'none';
-  }
 
   if (largeCta && 'IntersectionObserver' in window) {
     toggleSmallCta();
