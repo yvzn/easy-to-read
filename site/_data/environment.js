0802512b62a13160a75d9ddc060389762c4089d7
Yvan Razafindramanana <mail@ludeo.net>
2025-01-29 09:27:52 +0100

    Init user feedback buttons

--- site/_data/environment.js
+++ site/_data/environment.js
@@ -3,5 +3,6 @@ export default function () {
 		healthCheckUrl: process.env.HEALTH_CHECK_URL,
 		simplifiedTextUrl: process.env.SIMPLIFIED_TEXT_URL,
 		interactionUrl: process.env.INTERACTION_URL,
+		feedbackUrl: process.env.FEEDBACK_URL,
 	};
 }
--- site/eleventy.config.js
+++ site/eleventy.config.js
@@ -1,7 +1,11 @@
 export default function (eleventyConfig) {
-  eleventyConfig.addWatchTarget('./css/')
-  eleventyConfig.addWatchTarget('./js/')
-  eleventyConfig.addPassthroughCopy('./img/')
-  eleventyConfig.addWatchTarget('./img/')
-  eleventyConfig.addPassthroughCopy('.htaccess.sample')
+	eleventyConfig.addWatchTarget('./css/')
+	eleventyConfig.addWatchTarget('./js/')
+	eleventyConfig.addPassthroughCopy('./img/')
+	eleventyConfig.addWatchTarget('./img/')
+	eleventyConfig.addPassthroughCopy('.htaccess.sample')
+
+	eleventyConfig.setServerOptions({
+		liveReload: false,
+	})
 }
