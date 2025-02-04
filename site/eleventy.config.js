7afc1923de082051cc713603a9ea377d0c9048a7
Yvan Razafindramanana <mail@ludeo.net>
2025-02-04 08:58:55 +0100

    Local font faces

--- site/eleventy.config.js
+++ site/eleventy.config.js
@@ -1,4 +1,6 @@
-export default function (eleventyConfig) {
+import GetGoogleFonts from 'get-google-fonts';
+
+export default async function (eleventyConfig) {
 	eleventyConfig.addWatchTarget('./css/')
 	eleventyConfig.addWatchTarget('./js/')
 	eleventyConfig.addPassthroughCopy('./img/')
@@ -8,4 +10,17 @@ export default function (eleventyConfig) {
 	eleventyConfig.setServerOptions({
 		liveReload: false,
 	})
+
+	await eleventyConfig.addPlugin(getGoogleFonts);
+}
+
+async function getGoogleFonts(eleventyConfig) {
+	const ggf = new GetGoogleFonts()
+	await ggf.download('https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:wght@400;700&display=swap',
+		{
+			path: './fonts/',
+			overwriting: true,
+			verbose: true,
+		});
+	eleventyConfig.addPassthroughCopy('./fonts/');
 }
