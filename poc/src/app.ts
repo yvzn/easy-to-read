7afcc944101e58229b4985b5e133cd033203995f
Yvan Razafindramanana <mail@ludeo.net>
2026-03-04 09:16:48 +0100

    CORS

--- poc/src/app.ts
+++ poc/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import helmet from "helmet";
+import cors from "cors";
 import healthRouter from "./routes/health";
 import simplifiedRouter from "./routes/simplified";
 import feedbackRouter from "./routes/feedback";
@@ -8,9 +9,16 @@ import carbonFootprintRouter from "./routes/carbon-footprint";
 
 const app = express();
 
+// -- Middleware -----
+
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+	origin: process.env.CORS_ORIGINS?.split(",") || "http://localhost:8080",
+}))
+
+// -- Routes -----
 
 app.use("/api", healthRouter);
 app.use("/api", simplifiedRouter);
