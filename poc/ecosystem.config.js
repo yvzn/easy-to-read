f9fb719a5a08573c6434f6d7fecfa924714a0909
Yvan Razafindramanana <mail@ludeo.net>
2026-03-05 22:04:59 +0100

    POC Cleanup

--- poc/ecosystem.config.js
+++ poc/ecosystem.config.js
@@ -1,4 +1,5 @@
 // https://pm2.keymetrics.io/docs/usage/application-declaration/
+// az webapp config set --resource-group <resource-group-name> --name <app-name> --startup-file "pm2 start ecosystem.config.js --no-daemon"
 module.exports = {
   apps : [{
     name   : "api-express",
