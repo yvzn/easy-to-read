3fc6fae4d95e0969b36f7d914b9763df5aa26886
Yvan Razafindramanana <mail@ludeo.net>
2026-04-08 09:32:45 +0200

    Remove ExpressJS POC, non conclusive

--- poc/ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-// https://pm2.keymetrics.io/docs/usage/application-declaration/
-// az webapp config set --resource-group <resource-group-name> --name <app-name> --startup-file "pm2 start ecosystem.config.js --no-daemon"
-module.exports = {
-  apps : [{
-    name   : "api-express",
-    script : "./dist/server.js"
-  }]
-}
--- poc/src/routes/health.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-router.get("/health", (_req, res) => {
-	res.send("Healthy");
-});
-
-export default router;
